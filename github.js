ed0c7896e2be230a160fb7e8e8e45215406c4f91
Mitra Nami <a.mxnami@gmail.com>
2021-02-01 01:23:15 -0500

    Fetch repo info

--- github.js
+++ github.js
@@ -6,6 +6,8 @@ class GitHub {
         Authorization: `token ${my_token}`
       }
     };
+    this.repos_count = 4;
+    this.repos_sort = 'created: asc';
   
   }
 
@@ -14,12 +16,27 @@ class GitHub {
       `https://api.github.com/users/${user}`,
       this.config
     );
+    const reposResponse = fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`,
+      this.config
+    );
     
-    const profile = await (await profileResponse).json();
+   const responses = await Promise.all([
+     profileResponse,
+     reposResponse
+   ]);
+
+   if (responses.every(res => res.ok)) {
+     // if all responses have true for ok key
+     // get profile and repos objects
+    const [profile, repos] = await Promise.all(
+      responses.map(response => response.json())
+    );
+    return {profile, repos};
+   } else{
+    return {message: 'Not Found'};
+   }
     
-    return {
-      profile
-    };
   }
 
 }
